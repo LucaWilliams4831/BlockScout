03f3dbb55213364295d9b918a5ffe03bad0b8d16
LucaWilliams4831 <lucaw4831@gmail.com>
2023-10-01 21:36:42 +0000

    color changed

--- frontend/configs/app/api.ts
+++ frontend/configs/app/api.ts
@@ -2,16 +2,16 @@ import stripTrailingSlash from 'lib/stripTrailingSlash';
 
 import { getEnvValue } from './utils';
 
-const apiEndpoint = 'https://backendexplorer.ariettachain.tech'
+const apiEndpoint = 'https://backend.cosvm.net'
 
 console.log(apiEndpoint)
 
 const socketSchema = getEnvValue(process.env.NEXT_PUBLIC_API_WEBSOCKET_PROTOCOL) || 'wss';
 
 const api = Object.freeze({
-  host: 'backendexplorer.ariettachain.tech',
+  host: 'backend.cosvm.net',
   endpoint: apiEndpoint,
-  socket: `${ socketSchema }://backendexplorer.ariettachain.tech`,
+  socket: `${ socketSchema }://backend.cosvm.net`,
   basePath: '',
 });
 
--- frontend/ui/pages/Home.tsx
+++ frontend/ui/pages/Home.tsx
@@ -16,7 +16,7 @@ const Home = () => {
       <Box
         w="100%"
         style={{
-          background: 'linear-gradient(to right, gold, orange)'
+          background: 'linear-gradient(to right, #5d17ec, #0000FF)'
         }}
         borderRadius="24px"
         padding={{ base: '24px', lg: '48px' }}
